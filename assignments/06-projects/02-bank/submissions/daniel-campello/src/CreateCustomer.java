9bb7a32a2291d10fa3259aaa561678f06b54fe41
DanielCMBC <143235356+DanielCMBC@users.noreply.github.com>
2026-04-08 12:34:18 +0000

    limpo

---
+++ assignments/06-projects/02-bank/submissions/daniel-campello/src/CreateCustomer.java
@@ -0,0 +1,6 @@
+public abstract class CreateCustomer extends IDPBank {
+
+    public CreateCustomer(String firstName, String lastName, String CPF, int ID) {
+        super(firstName, lastName, CPF);
+    }
+}
---
+++ assignments/06-projects/02-bank/submissions/daniel-campello/src/IDPBank.java
@@ -0,0 +1,47 @@
+public abstract class IDPBank { // Removed 'abstract' for direct instantiation in main; reconsider if abstract is truly intended.
+    private String firstName;
+    private String lastName;
+    private String CPF;
+    private int ID;
+    private static int clientCount; 
+    private static int nextCount = 1;
+    
+    public IDPBank(String firstName, String lastName, String CPF) { // This is now a proper constructor
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.CPF = CPF;
+        this.ID = nextCount++;
+        clientCount++;
+    }
+
+    public static int getClientCount() {
+        return clientCount;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCPF() {
+        return this.CPF;
+    }
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public abstract void CreateCustomer(); 
+}
